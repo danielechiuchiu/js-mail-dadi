059dad603c6a195220057a7d40e8c7b3e653cded
danielechiuchiu <daniele.chiuchiu@gmail.com>
2020-07-08 14:09:49 +0200

    final commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var email = prompt('fornisci la tua email' )
+var emailsistema = [ "mario.rossi@gmail.com" , "maria.rossi@gmail.com"]
+
+for (var i = 0; i < emailsistema.length; i++) {
+  if (emailsistema[i] == email) {
+  document.getElementById("esercizio").innerHTML = "Puoi accedere";
+} else{
+  document.getElementById("esercizio").innerHTML = "L' accesso non è autorizzato";
+}
+}
