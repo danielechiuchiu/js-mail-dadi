f127af7abc3df2e1ee1ac47d078eb54fbbd9ed7a
danielechiuchiu <daniele.chiuchiu@gmail.com>
2020-07-08 15:33:58 +0200

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var email = prompt('fornisci la tua email' )
-var emailsistema = [ "mario.rossi@gmail.com" , "maria.rossi@gmail.com"]
+var email = prompt('fornisci la tua email' );
+var emailsistema = ["mario.rossi@gmail.com", "maria.rossi@gmail.com", "mario.bianchi@gmail.com"];
+var trovata = false;
 
-for (var i = 0; i < emailsistema.length-1; i++) {
-  if (emailsistema[i] == email) {
-  document.getElementById("esercizio").innerHTML = "Puoi accedere";
-} else{
-  document.getElementById("esercizio").innerHTML = "L' accesso non è autorizzato";
+for (var i = 0; i < emailsistema.length; i++) {
+if (emailsistema[i] == email) {
+  trovata= true;
+}
+
+
+if (trovata == true) {
+document.getElementById("esercizio").innerHTML = "Accesso consentito";
+} else {
+  document.getElementById('esercizio').innerHTML = "Non hai le autorizzazioni per accedere";
 }
 }
