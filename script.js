a678d9ed226f1a2128a0a89ef2eb3e9d26819d52
danielechiuchiu <daniele.chiuchiu@gmail.com>
2020-07-08 14:14:52 +0200

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var email = prompt('fornisci la tua email' )
 var emailsistema = [ "mario.rossi@gmail.com" , "maria.rossi@gmail.com"]
 
-for (var i = 0; i < emailsistema.length; i++) {
+for (var i = 0; i < emailsistema.length-1; i++) {
   if (emailsistema[i] == email) {
   document.getElementById("esercizio").innerHTML = "Puoi accedere";
 } else{
