21296cdbf56cba8e38927bb29d93e360351dcde4
danielechiuchiu <daniele.chiuchiu@gmail.com>
2020-07-09 14:42:10 +0200

    commit

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-var email = prompt('fornisci la tua email' );
-var emailsistema = ["mario.rossi@gmail.com", "maria.rossi@gmail.com", "mario.bianchi@gmail.com"];
-var trovata = false;
+// var email = prompt('fornisci la tua email' );
+// var emailsistema = ["mario.rossi@gmail.com", "maria.rossi@gmail.com", "mario.bianchi@gmail.com"];
+// var trovata = false;
+//
+// for (var i = 0; i < emailsistema.length; i++) {
+// if (emailsistema[i] == email) {
+//   trovata= true;
+// }
+// }
+//
+// if (trovata == true) {
+// document.getElementById("esercizio").innerHTML = "Accesso consentito";
+// } else {
+// document.getElementById('esercizio').innerHTML = "Non hai le autorizzazioni per accedere";
+// }
 
-for (var i = 0; i < emailsistema.length; i++) {
-if (emailsistema[i] == email) {
-  trovata= true;
-}
-}
+var dadoutente = Math.floor(Math.random()*6)+1
+console.log(dadoutente)
 
-if (trovata == true) {
-document.getElementById("esercizio").innerHTML = "Accesso consentito";
-} else {
-  document.getElementById('esercizio').innerHTML = "Non hai le autorizzazioni per accedere";
-}
+var dadocomputer = Math.floor(Math.random()*6)+1
+console.log(dadocomputer)
+
+
+if (dadoutente > dadocomputer) {
+  var risultato = 'ha vinto l\' utente'
+} else
+  var risultato = 'ha vinto il computer'
+document.getElementById("esercizio").innerHTML= risultato;
